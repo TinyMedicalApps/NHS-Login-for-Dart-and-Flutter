4ad578b43a19d182fd536059080a588f4b36a097
Razvan Cristian Lung <lung.razvan@gmail.com>
2019-04-30 14:04:18 +0300

    remove deprecation annotation

--- lib/src/models/authentication/nhs_scope.dart
+++ lib/src/models/authentication/nhs_scope.dart
@@ -31,27 +31,16 @@ class NhsScope {
 
   /// This scope value requests access to the [NhsUserinfo.address] claim as
   /// held within the NHS Personal Demographics Service
-  @Deprecated(
-      'Support for the address scope is currently under user research and '
-      'evaluation.')
   static const NhsScope address = NhsScope._('address');
 
   /// This scope value requests access to the End-User's
   /// [NhsUserinfo.gpIntegrationCredentials] claims
-  @Deprecated(
-      'Support for the gp_integration_credentials scope is currently under '
-      'user research and evaluation.')
-  static const NhsScope gpIntegrationCredentials =
-      NhsScope._('gp_integration_credentials');
+  static const NhsScope gpIntegrationCredentials = NhsScope._('gp_integration_credentials');
 
   /// This scope value requests access to the End-User’s
   /// [NhsUserinfo.gpRegistrationDetails] claims as held within the NHS Personal
   /// Demographics Service
-  @Deprecated(
-      'Support for the gp_integration_credentials scope is currently under '
-      'user research and evaluation.')
-  static const NhsScope gpRegistrationDetails =
-      NhsScope._('gp_registration_details');
+  static const NhsScope gpRegistrationDetails = NhsScope._('gp_registration_details');
 
   /// This scope value requests access to the End-User’s additional demographics
   /// claims (as held within the NHS Personal Demographics Service), which are:
@@ -95,14 +84,12 @@ class NhsScopeSerializer implements PrimitiveSerializer<NhsScope> {
   final String wireName = 'NhsScope';
 
   @override
-  Object serialize(Serializers serializers, NhsScope error,
-      {FullType specifiedType = FullType.unspecified}) {
+  Object serialize(Serializers serializers, NhsScope error, {FullType specifiedType = FullType.unspecified}) {
     return error._value;
   }
 
   @override
-  NhsScope deserialize(Serializers serializers, Object serialized,
-      {FullType specifiedType = FullType.unspecified}) {
+  NhsScope deserialize(Serializers serializers, Object serialized, {FullType specifiedType = FullType.unspecified}) {
     return NhsScope.values[NhsScope._names.indexOf(serialized)];
   }
 }
